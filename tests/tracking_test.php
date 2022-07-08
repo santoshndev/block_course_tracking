f8bc27938f8946fb8764dcca4237ec68a009972a
Santosh Nagargoje <santosh.nag2217@gmail.com>
2022-07-08 10:51:50 +0530

    minor changes in the code

--- tests/tracking_test.php
+++ tests/tracking_test.php
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 namespace block_course_tracking;
+use block_course_tracking;
+use context_course;
 /**
  * PHPUnit block_course_tracking tests
  *
